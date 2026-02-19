7c82dbdfb36f807c15313223ae3b6b5e7487d723
oochomba <oochomba@gmail.com>
2026-02-19 09:27:54 +0300

    Navbar Changes

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { navbarStyles } from '../assets/dummyStyles'
+
+function Navbar() {
+  return (
+    <>
+      <div className={navbarStyles.navbarBorder}>
+      </div>
+<nav className={`${navbarStyles.navbarContainer} ${
+
+}`}>
+
+</nav>
+
+    </>
+  )
+}
+
+export default Navbar
